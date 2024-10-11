742ab9c91182827ba5d5736d1f83617b878dc616
Tejas Gund <tejasgund111@gmail.com>
2024-10-11 22:22:45 +0530

    Updated

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -43,7 +43,7 @@ function ForgotPassword() {
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   placeholder="Enter email address"
-                  className="form-style w-full"
+                  className="w-full rounded-[0.5rem] bg-richblack-800 p-[12px] pr-12 text-richblack-5"
                 />
               </label>
             )}
