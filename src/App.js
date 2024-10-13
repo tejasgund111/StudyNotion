ade3cc8a8660460f5f5bb4d24af42fd81b822305
Tejas Gund <tejasgund111@gmail.com>
2024-10-13 18:30:15 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import ForgotPassword from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
 import VerifyEmail from "./pages/VerifyEmail";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
+
 
 function App() {
   return (
@@ -69,6 +71,16 @@ function App() {
             </OpenRoute>
           }
         />
+
+        <Route
+          path="contact"
+          element={
+            <OpenRoute>
+              <Contact />
+            </OpenRoute>
+          }
+        />
+
 
 
       </Routes>
