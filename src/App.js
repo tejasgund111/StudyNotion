d0d670a1bda056e3994c29ac562039ef9eed3c52
Tejas Gund <tejasgund111@gmail.com>
2024-10-14 15:32:40 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -16,8 +16,15 @@ import Dashboard from "./pages/Dashboard"
 import PrivateRoute from "./components/core/Auth/PrivateRoute";
 import Error from "./pages/Error"
 import Settings from "./components/core/Dashboard/Settings/index";
+import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses";
+import Cart from "./components/core/Dashboard/Cart";
+import { ACCOUNT_TYPE } from "./utils/constants";
+import { useSelector } from "react-redux";
 
 function App() {
+
+  const { user } = useSelector((state) => state.profile)
+
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Navbar />
@@ -84,6 +91,19 @@ function App() {
           {/* Route for all users */}
           <Route path="dashboard/my-profile" element={<MyProfile />} />
           <Route path="dashboard/Settings" element={<Settings />} />
+
+          {/* Route only for Students */}
+          {
+            user?.accountType === ACCOUNT_TYPE.STUDENT && (
+              <>
+                <Route
+                  path="dashboard/enrolled-courses"
+                  element={<EnrolledCourses />}
+                />
+                <Route path="/dashboard/cart" element={<Cart />} />
+              </>
+            )
+          }
         </Route>
 
 
