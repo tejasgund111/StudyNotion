dc855792458dab59c68af361a2ed86c50d501cff
Tejas Gund <tejasgund111@gmail.com>
2024-10-18 00:39:17 +0530

    Updated for CatalogPage

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ import { useSelector } from "react-redux";
 import AddCourse from "./components/core/Dashboard/AddCourse";
 import MyCourses from "./components/core/Dashboard/MyCourses";
 import EditCourse from "./components/core/Dashboard/EditCourse/index";
+import Catalog from "./pages/Catalog";
 
 function App() {
 
@@ -35,6 +36,8 @@ function App() {
         <Route path="/" element={<Home />} />
 
         <Route path="about" element={<About />} />
+
+        <Route path="catalog/:catalogName" element={<Catalog />} />
 
         <Route path="contact" element={<Contact />} />
 
