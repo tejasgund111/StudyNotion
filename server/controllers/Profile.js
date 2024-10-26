afc18b025cf8bbcb98551332068d7a6ce11659fd
Tejas Gund <tejasgund111@gmail.com>
2024-10-26 12:00:52 +0530

    Updated

--- server/controllers/Profile.js
+++ server/controllers/Profile.js
@@ -1,5 +1,6 @@
 const Profile = require("../models/Profile");
 const User = require("../models/User");
+const Course = require("../models/Course");
 const { uploadImageToCloudinary } = require("../utils/imageUploader");
 const CourseProgress = require("../models/CourseProgress")
 const { convertSecondsToDuration } = require("../utils/secToDuration");
@@ -217,5 +218,33 @@ exports.getEnrolledCourses = async (req, res) => {
 			success: false,
 			message: error.message,
 		})
+	}
+}
+
+exports.instructorDashboard = async(req, res) => {
+	try{
+		const courseDetails = await Course.find({instructor:req.user.id});
+
+		const courseData  = courseDetails.map((course)=> {
+			const totalStudentsEnrolled = course.studentsEnrolled.length
+			const totalAmountGenerated = totalStudentsEnrolled * course.price
+
+			//create an new object with the additional fields
+			const courseDataWithStats = {
+				_id: course._id,
+				courseName: course.courseName,
+				courseDescription: course.courseDescription,
+				totalStudentsEnrolled,
+				totalAmountGenerated,
+			}
+			return courseDataWithStats
+		})
+
+		res.status(200).json({courses:courseData});
+
+	}
+	catch(error) {
+		console.error(error);
+		res.status(500).json({message:"Internal Server Error"});
 	}
 }
